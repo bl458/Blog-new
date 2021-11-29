a98e4e31718c227bbd0f544b1c33877cc8659d36
Byungchan Lim <bl458@cornell.edu>
2021-11-29 16:45:17 -0500

    FIX feed style

--- src/pages/feed/Feed.js
+++ src/pages/feed/Feed.js
@@ -4,19 +4,21 @@ import "./Feed.css";
 
 const FEED_DATA = [
   {
-    title: "TITLE",
-    titleSub: "title sub",
-    img: { src: "https://picsum.photos/200/300", width: 200, height: 300 },
+    title: "CSS Is Finally Getting If/Else Statements!",
+    titleSub:
+      "When I started coding CSS in 2011 (wow) I could never have suspected how much the language would change. I still remember using PIE.htc to…",
+    img: { src: "https://picsum.photos/200/200", width: 300, height: 300 },
     createdAt: new Date("2021-11-29"),
   },
   {
-    title: "TITLE2",
-    titleSub: "title sub2",
-    img: { src: "https://picsum.photos/300/200", width: 300, height: 200 },
+    title: "GitHub Commit Bot to Stack Your Contributions Graph",
+    titleSub: "How to become the guy she tells you not to worry about",
+    img: { src: "https://picsum.photos/300/300", width: 200, height: 200 },
     createdAt: new Date("2021-10-20"),
   },
 ];
 
+/* TBI Make feed item size responsive to amount of text */
 export default class Feed extends React.Component {
   render() {
     return (
@@ -26,9 +28,12 @@ export default class Feed extends React.Component {
             {FEED_DATA.map((f, i) => (
               <div className="feed_item" key={i}>
                 <div className="feed_title_wrap">
-                  <p>{f["title"]}</p>
-                  <p>{f["titleSub"]}</p>
-                  <p>{f["createdAt"].toDateString()}</p>
+                  <p className="feed_title">{f["title"]}</p>
+                  <p className="feed_titleSub">{f["titleSub"]}</p>
+                  <p className="feed_createdAt">
+                    {/* TBI */}
+                    {f["createdAt"].toDateString()}
+                  </p>
                 </div>
                 <div className="feed_img_wrap">
                   <img src={f["img"]["src"]} alt="" className="feed_img"></img>
@@ -38,10 +43,10 @@ export default class Feed extends React.Component {
           </div>
 
           <div className="feed_about">
-            <img></img>
+            <img alt=""></img>
             <p></p>
             <div className="feed_about_links">
-              <img></img>
+              <img alt=""></img>
             </div>
           </div>
         </div>
