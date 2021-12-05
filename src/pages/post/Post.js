c572630fc121a2ca3e99ac0c66bf192ee93ee0b5
Byungchan Lim <bl458@cornell.edu>
2021-12-05 17:08:44 -0500

    FEAT implement basics for post page

--- src/pages/post/Post.js
+++ src/pages/post/Post.js
@@ -1,12 +1,29 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 
+import "./Post.css";
+
+/* 
+  TBI Add scrollup button, flow diagram  
+*/
 function Post(props) {
   const { state } = useLocation();
   const { post } = state;
   console.log(post);
 
-  return <div>This is a post</div>;
+  return (
+    <div className="post">
+      <div className="post_inner">
+        <p className="post_title">{post["title"]}</p>
+        <p className="post_titleSub">{post["titleSub"]}</p>
+        <p className="post_createdAt">{post["createdAt"].toDateString()}</p>
+        {/* TBI need styled components to make img width dynamic based on aspect ratio */}
+        <img src={post["img"]["src"]} alt="" className="post_img" />
+
+        <p className="post_content"></p>
+      </div>
+    </div>
+  );
 }
 
 export default Post;
