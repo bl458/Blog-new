398268d55ef6ecfccd7e8795689acffb7c68e9d5
Byungchan Lim <bl458@cornell.edu>
2021-12-28 11:39:27 +0900

    FEAT implement postEdit btns

--- src/pages/postEdit/PostEdit.js
+++ src/pages/postEdit/PostEdit.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 
 import { isPostsPath } from "../../helper/miscHelper";
@@ -7,6 +8,9 @@ import "./PostEdit.css";
 function PostEdit() {
   const { state } = useLocation();
   const { prevPath } = state || {};
+
+  const [postImg, setPostImg] = useState();
+
   const getTitle = () => {
     if (isPostsPath(prevPath)) {
       return "Edit Post";
@@ -15,15 +19,44 @@ function PostEdit() {
     }
   };
 
+  const onUploadImg = (event) => {
+    setPostImg(event.target.files[0]);
+  };
+
+  //TBI
+  const onSubmit = () => {};
+
   return (
     <div>
       <h1 className="postEdit__title">{getTitle()}</h1>
       <h2 className="postEdit__titleSub">Title</h2>
       <input type="text" className="postEdit__titleInput" />
+
       <h2 className="postEdit__titleSub">Image</h2>
-      <button className="postEdit__uploadBtn">Upload</button>
+      {postImg && (
+        <>
+          <img
+            src={URL.createObjectURL(postImg)}
+            alt=""
+            className="postEdit__postImg"
+          />
+          <br />
+          <br />
+        </>
+      )}
+      <input
+        type="file"
+        name="myImage"
+        accept="image/png, image/gif, image/jpeg"
+        onChange={onUploadImg}
+      />
+
       <h2 className="postEdit__titleSub">Content</h2>
       <textarea rows="40" className="postEdit__contentInput"></textarea>
+
+      <button className="postEdit__submitBtn" onClick={onSubmit}>
+        Submit
+      </button>
     </div>
   );
 }
